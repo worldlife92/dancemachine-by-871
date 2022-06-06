29da83818da343ec3c438041d8835fa12f240a83
DucVan Ngo <duc@ngo-familie.de>
2022-06-06 10:12:55 +0200

    Updated with package function for model

--- dancemachine_by_871/api.py
+++ dancemachine_by_871/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dancemachine_by_871.data_preproc import Preprocessor
 from dancemachine_by_871.gcp import get_urls
+from dancemachine_by_871.model import DanceModel
 
 
 
@@ -10,12 +11,13 @@ app = FastAPI()
 def index():
     return {"ok": True}
 
-@app.get('/predict-test')
+@app.get('/predict')
 def predict(filename):
 
     #Receive filename from frontend
     #Function should call the Videofile name from Google bucket
     preproc = Preprocessor(get_urls(filename))
     X_0, y_0, maxlen = preproc.extract_X_y_angles(0)
-    X, y = preproc.create_X_y([X_0], [y_0], maxlen)
+    model = DanceModel("model4")
+    model.predict(X_0)
     return {'ok': True}
---
+++ dancemachine_by_871/model.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.models import load_model
+
+class DanceModel:
+
+    def __init__(self, path):
+        self.path = path
+        self.model = load_model(self.path)
+        return self.model
+
+    def predict(self, X_test_preproc):
+        return self.model.predict(X_test_preproc)

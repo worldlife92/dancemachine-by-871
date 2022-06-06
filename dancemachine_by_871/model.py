d7179fad9c1ddc92df6f69183e9a4fc65fc580e3
DucVan Ngo <duc@ngo-familie.de>
2022-06-06 12:06:34 +0200

    Model with 65% accuracy

--- dancemachine_by_871/model.py
+++ dancemachine_by_871/model.py
@@ -5,7 +5,12 @@ class DanceModel:
     def __init__(self, path):
         self.path = path
         self.model = load_model(self.path)
-        return self.model
 
     def predict(self, X_test_preproc):
         return self.model.predict(X_test_preproc)
+
+
+
+if __name__ == '__main__':
+    model = DanceModel('model3')
+    print('testtesttest')

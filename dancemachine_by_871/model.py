f9db58c8f19b99d0d35cbcd97746a73f58fe8271
DucVan Ngo <duc@ngo-familie.de>
2022-06-06 13:50:44 +0200

    Updated with trained model to load

--- dancemachine_by_871/model.py
+++ dancemachine_by_871/model.py
@@ -1,16 +1,28 @@
 from tensorflow.keras.models import load_model
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, LSTM, GRU, Masking, Dropout
+from tensorflow.keras import regularizers
+import numpy as np
+
+MASKING = np.zeros((10,))
+MASKING[:] = -20
+REG_l12 = regularizers.L1L2(0.1, 0.1)
+PATH = 'training1/cp.ckpt'
 
 class DanceModel:
 
-    def __init__(self, path):
-        self.path = path
-        self.model = load_model(self.path)
+    def __init__(self, maxlen_pad=400):
+        self.model = Sequential([Masking(mask_value=MASKING, input_shape=(maxlen_pad,10)),
+                    LSTM(30, return_sequences=True),
+                    GRU(30),
+                    Dropout(0.2),
+                    Dense(10, kernel_regularizer=REG_l12),
+                    Dense(1, activation='sigmoid')])
 
     def predict(self, X_test_preproc):
+        self.model.load_weights(PATH)
         return self.model.predict(X_test_preproc)
 
-
-
 if __name__ == '__main__':
-    model = DanceModel('model3')
+    model = DanceModel()
     print('testtesttest')
